e9b47e6d1ccad0b06091bdc1ab6b2dda7bd24d12
Craig Johnson <craig_s_johnson@icloud.com>
2022-09-19 15:08:48 -0400

    import from dict for json serialization

--- src/models/models.py
+++ src/models/models.py
@@ -1,29 +1,24 @@
+import json
 from typing import List, Pattern as RegexPattern
 
-class Article:
+class Article(dict):
     def __init__(self, id, title, content, published):
-        self.id = id
-        self.title = title
-        self.content = content
-        self.published = published
-        
-    def __repr__(self):
-        return f"Article({self.id}, {self.title}, {self.content}, {self.published})"
+        dict.__init__(self, id=id, title=title, content=content, published=published)
     
-    def to__json__(self):
+    def __to_json__(self):
         return {
             "id": self.id,
             "title": self.title,
             "content": self.content,
             "published": self.published
         }
-class Pattern:
+             
+class Pattern(dict):
     def __init__(self, id: str, pattern: RegexPattern):
-        self.id = id
-        self.pattern = pattern
+        dict.__init__(self, id=id, pattern=pattern)
         
     def __repr__(self):
-        return f"Pattern(id={self.id}, pattern={self.pattern.pattern})"
+        return json.dumps(self.__dict__)
     
     def __to_json__(self):
         return {
@@ -31,18 +26,6 @@ class Pattern:
             "pattern": self.pattern.pattern
         }
 
-class NewsSource:
+class NewsSource(dict):
     def __init__(self, id: str, url: str, patterns: List[str]=None):
-        self.id = id
-        self.url = url
-        self.patterns = patterns
-        
-    def __repr__(self):
-        return f"Feed(id={self.id}, url={self.url}, patterns={self.patterns})"
-    
-    def __to_json__(self):
-        return {
-            "id": self.id,
-            "url": self.url,
-            "patterns": self.patterns
-        }
+        dict.__init__(self, id=id, url=url, patterns=patterns)
