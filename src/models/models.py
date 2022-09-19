01d009fcb26c64354dfe583d93f8f133e847ef59
Craig Johnson <craig_s_johnson@icloud.com>
2022-09-19 15:14:48 -0400

    remove json/repr

--- src/models/models.py
+++ src/models/models.py
@@ -4,27 +4,10 @@ from typing import List, Pattern as RegexPattern
 class Article(dict):
     def __init__(self, id, title, content, published):
         dict.__init__(self, id=id, title=title, content=content, published=published)
-    
-    def __to_json__(self):
-        return {
-            "id": self.id,
-            "title": self.title,
-            "content": self.content,
-            "published": self.published
-        }
              
 class Pattern(dict):
     def __init__(self, id: str, pattern: RegexPattern):
         dict.__init__(self, id=id, pattern=pattern)
-        
-    def __repr__(self):
-        return json.dumps(self.__dict__)
-    
-    def __to_json__(self):
-        return {
-            "id": self.id,
-            "pattern": self.pattern.pattern
-        }
 
 class NewsSource(dict):
     def __init__(self, id: str, url: str, patterns: List[str]=None):
