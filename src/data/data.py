6adf4e2f9365e37500facd3a17a87a216e249300
Craig Johnson <craig_s_johnson@icloud.com>
2022-09-19 14:31:59 -0400

    add some RSS sources and dummy Article data

--- src/data/data.py
+++ src/data/data.py
@@ -1,7 +1,12 @@
 from typing import List, Dict, Pattern as RegexPattern
-from models.models import NewsSource, Pattern
+from models.models import Article, NewsSource, Pattern
 import re
 
+articles: Dict[str, Article] = {
+    "1": Article("1", "Title 1", "Content 1", "2020-01-01"),
+    "2": Article("2", "Title 2", "Content 2", "2020-01-02"),
+    "3": Article("3", "Title 3", "Content 3", "2020-01-03"),
+}
 
 patterns: Dict[str, Pattern] = {
     "listings" : Pattern(
@@ -19,17 +24,33 @@ patterns: Dict[str, Pattern] = {
 }
 
 news_sources: Dict[str, NewsSource] = {
-    "bankless": NewsSource(
-        id = "bankless",
-        url = "https://www.banklesshq.com/feed/podcast/",
+    "decrypt": NewsSource(
+        id = "decrypt",
+        url = "https://decrypt.co/feed",
     ),
-    "a16z": NewsSource(
-        id = "a16z",
-        url = "https://a16z.com/feed/podcast/",
+    "blockworks": NewsSource(
+        id= "blockworks",
+        url= "https://blockworks.co/feed",
     ),
-    "defipulse": NewsSource(
-        id =  "defipulse",
-        url = "https://defipulse.com/blog/feed/",
-    )
+    "cryptopotato": NewsSource(
+        id=  "cryptopotato",
+        url= "https://cryptopotato.com/feed",
+    ),
+    "cryptobriefing": NewsSource(
+        id= "cryptobriefing",
+        url= "https://cryptobriefing.com/feed",
+    ),
+    "dailyhodl": NewsSource(
+        id= "dailyhodl",
+        url= "https://dailyhodl.com/feed",
+    ),
+    "cointelegraph": NewsSource(
+        id= "cointelegraph",
+        url= "https://cointelegraph.com/rss",
+    ),
+    "coindesk": NewsSource(
+        id = "coindesk",
+        url= "https://www.coindesk.com/arc/outboundfeeds/rss/",
+    ),
 }
 
