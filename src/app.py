1259ad5087cb311f5fbfcc4aad7914130ed27c7c
Craig Johnson <craig_s_johnson@icloud.com>
2022-09-19 14:31:04 -0400

    add Flask blueprints

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from routes.news_source import app_news_source
 from routes.pattern import app_pattern
+from routes.articles import app_articles
 
 # Initalize Flask App
 app = Flask(__name__)
@@ -13,3 +14,4 @@ def hello_world():
 # Register Blueprints
 app.register_blueprint(app_news_source)
 app.register_blueprint(app_pattern)
+app.register_blueprint(app_articles)
