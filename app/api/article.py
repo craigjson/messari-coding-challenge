5bed1218a01ada4fc2c011990f771e008ba1efb1
Craig Johnson <craig_s_johnson@icloud.com>
2022-09-20 17:26:17 -0400

    move mock_data to script

---
+++ app/api/article.py
@@ -0,0 +1,16 @@
+from typing import Dict, List
+from data.models.article import  Article
+from data.query import query_articles, query_artcile, delete_article
+
+# Get all articles
+def get_all_articles() -> List[Article]:
+    return query_articles()
+
+# Get Article By Id
+def get_article_by_id(article_id: str) -> Article:
+    return query_artcile(article_id)
+    
+# Delete Article by Id
+def delete_article(article_id: str):
+    return delete_article(article_id)
+    
--- app/scripts/load_mock_data.py
+++ app/scripts/load_mock_data.py
@@ -1,8 +1,61 @@
 from datetime import date
 
 from data.postgres import Base, Session, engine
-from data.mock_data import articles, news_sources, patterns
+from data.models.article import Article
+from data.models.pattern import Pattern
+from data.models.news_source import NewsSource
 
+articles = [
+    Article(id="1", title="Title 1", content="Content 1", link="link1", published="2020-01-01"),
+    Article(id="2", title="Title 2", content="Content 2", link="link2", published="2020-01-02"),
+    Article(id="3", title="Title 3", content="Content 3", link="link3", published="2020-01-03")
+    ]
+
+patterns = [
+    Pattern(
+        id = "listings",
+        pattern = ".*((Coinbase)|(Binance)).*list.*"
+    ),
+    Pattern(
+        id = "hacks",
+        pattern = ".*((hack)|(exploit)|(vuln)).*"
+    ),
+    Pattern(
+        id = "eth_news",
+        pattern = ".*((Ethereum)|(ETH)).*((fork)|(upgrad)).*"
+    )
+]
+
+news_sources = [
+    NewsSource(
+        id = "decrypt",
+        url = "https://decrypt.co/feed",
+    ),
+    NewsSource(
+        id= "blockworks",
+        url= "https://blockworks.co/feed",
+    ),
+    NewsSource(
+        id=  "cryptopotato",
+        url= "https://cryptopotato.com/feed",
+    ),
+    NewsSource(
+        id= "cryptobriefing",
+        url= "https://cryptobriefing.com/feed",
+    ),
+    NewsSource(
+        id= "dailyhodl",
+        url= "https://dailyhodl.com/feed",
+    ),
+    NewsSource(
+        id= "cointelegraph",
+        url= "https://cointelegraph.com/rss",
+    ),
+    NewsSource(
+        id = "coindesk",
+        url= "https://www.coindesk.com/arc/outboundfeeds/rss/",
+    ),
+]
 
 def load_mock_data():
     # Generate DB Schema
