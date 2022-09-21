cde6734ba3374443c5172f4d152d57b1ea7cb7fd
Craig Johnson <craig_s_johnson@icloud.com>
2022-09-21 10:57:51 -0400

    Use Redis instead of in memory set for URL cache

--- app/util/url_cache.py
+++ app/util/url_cache.py
@@ -1,4 +1,10 @@
-from datetime import datetime
-from typing import Set
+from redis import Redis
 
-url_visited_set: Set[str] = set()
+redis = Redis(host='0.0.0.0', port=6379)
+cache_name = 'url_visited_cache'
+
+def is_containted_in_url_visited_cache(url: str) -> bool:
+    return redis.sismember(cache_name, url)
+
+def add_to_url_visited_cache(url: str):
+    redis.sadd(cache_name, url)
