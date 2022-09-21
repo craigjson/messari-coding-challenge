7923c93d35e473102dd547a72c485c5e9ca1effa
Craig Johnson <craig_s_johnson@icloud.com>
2022-09-21 11:00:57 -0400

    add basic error logging for c ache

--- app/util/url_cache.py
+++ app/util/url_cache.py
@@ -1,10 +1,21 @@
 from redis import Redis
 
-redis = Redis(host='0.0.0.0', port=6379)
 cache_name = 'url_visited_cache'
 
+try:
+    redis = Redis(host='redis', port=6379)
+except Exception as e:
+    print(f"Failed to connect to Redis with error: {e}")
+    
 def is_containted_in_url_visited_cache(url: str) -> bool:
-    return redis.sismember(cache_name, url)
+    try:
+        return redis.sismember(cache_name, url)
+    except Exception as e:
+        print(f"Failed to check if URL is contained in cache with error: {e}")
+        return False
 
 def add_to_url_visited_cache(url: str):
-    redis.sadd(cache_name, url)
+    try:
+        redis.sadd(cache_name, url)
+    except Exception as e:
+        print(f"Failed to add URL to cache with error: {e}")
