f110b79ffd688b648fdd1551117b1f8dcf9441ff
Craig Johnson <craig_s_johnson@icloud.com>
2022-09-21 14:54:30 -0400

    resolve merge entirely

--- app/api/article.py
+++ app/api/article.py
@@ -17,7 +17,3 @@ def get_article_by_id(article_id: str) -> Article:
 # Delete Article by Id
 def delete_article(article_id: str):
     return delete_article(article_id)
-
-def get_articles_with_no_match() -> List[Article]:
-    return query_articles_without_matches()
-    
