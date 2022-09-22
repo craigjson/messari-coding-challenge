f55386e2a56b067c6a3c705f2c36e6ecfca26445
Craig Johnson <craig_s_johnson@icloud.com>
2022-09-22 01:07:26 -0400

    update ETL to publish articles to subscription channels

--- app/etl/pattern_match.py
+++ app/etl/pattern_match.py
@@ -5,12 +5,15 @@ from data.models.article import Article
 from data.models.article_pattern_match import ArticlePatternMatch
 from data.models.pattern import Pattern
 from data.query import query_patterns, save_article_pattern_match
+from api.stream import publish_article_to_stream
 
 
 # Determine if an article matches a pattern, save the match to the database
 def article_content_matches_pattern(article: Article, pattern: Pattern) -> bool:
     if search(pattern.pattern, article.content):
             save_article_pattern_match(ArticlePatternMatch(article_id=article.id, pattern_id=pattern.id))
+            log_article_to_console(article)
+            publish_article_to_stream(article)
             return True
     return False
 
@@ -21,3 +24,9 @@ def article_content_matches_any_pattern(article: Article) -> List[str]:
         if article_content_matches_pattern(article, pattern):
             return True
     return False
+
+def log_article_to_console(article: Article):
+    print()
+    print(f"Title: {article.title}")
+    print(f"Published: {article.published}")
+    print(f"Content: {article.content}")
