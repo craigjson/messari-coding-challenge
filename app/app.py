2b6253b0a26829fcc869d558d7945d0ea8242dcc
Craig Johnson <craig_s_johnson@icloud.com>
2022-09-22 03:35:18 -0400

    move scripts to util file

--- app/app.py
+++ app/app.py
@@ -8,8 +8,8 @@ from routes.etl import app_etl
 from routes.news_source import app_news_source
 from routes.pattern import app_pattern
 from routes.stream import app_stream
-from scripts.setup_db import create_db_tables
-
+from util.scripts.setup_db import create_db_tables
+    
 # Attempt to load .env file
 load_dotenv()
 
